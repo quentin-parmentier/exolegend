d351738aba567e18fe910991dfceac032def430f
Jean-Baptiste Guéry <jean-baptiste.guery@telecomnancy.net>
2024-02-18 00:08:41 +0100

    débug simplify

--- src/NavigationStack.cpp
+++ src/NavigationStack.cpp
@@ -48,19 +48,18 @@ void NavigationStack::reset()
 
 void NavigationStack::simplify()
 {
-    // MyPosition *ref = positions[0];
     std::cout << "start SIpmlify \n";
-    // MyPosition *positionsTemp[stackSize] ;
     positionsTemp[0] = positions[0];
     int tableposition = 0;
 
     // iterate over X
-    for (int i = 1; i < stackSize - 1; i++)
+    for (int i = 1; i <= currentIndex - 1; i++)
     {
         // std::cout << "iteration" << i <<"\n";
         // si le point n'a pas le même X que ces voisins
         if ((positions[i - 1]->getX() == positions[i]->getX() && positions[i + 1]->getX() == positions[i]->getX() && abs(positions[i + 1]->getY() - positions[i - 1]->getY()) != 0) || (positions[i - 1]->getY() == positions[i]->getY() && positions[i + 1]->getY() == positions[i]->getY() && abs(positions[i + 1]->getX() - positions[i - 1]->getX()) != 0))
         {
+            std::cout << "ignore x " << positions[i]->getX() << " y " << positions[i]->getY() << "\n";
             // tableposition++;
             // positionsTemp[tableposition] = positions[i];
         }
@@ -70,6 +69,8 @@ void NavigationStack::simplify()
             positionsTemp[tableposition] = positions[i];
         }
     }
+    tableposition++;
+    positionsTemp[tableposition] = positions[currentIndex];
     currentIndex = tableposition;
     for (int i = 0; i <= currentIndex; i++)
     {
