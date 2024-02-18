b7e681fc85bda04f52d1320fae62195f75d49964
Alex Gabriel <alex.gabriel@univ-lorraine.fr>
2024-02-18 01:38:02 +0100

    fix defend

--- src/RobotsData.cpp
+++ src/RobotsData.cpp
@@ -35,6 +35,9 @@ void RobotsData::init()
     for (uint8_t id : gladiator->game->getPlayingRobotsId().ids)
     {
         int advers = 0;
+        comp[0] = 0;
+        comp[1] =1;
+        coop[0]=0;
         RobotData ret{};
         if (id != coop[0])
         {
@@ -60,13 +63,13 @@ void RobotsData::init()
 RobotsData::isEnemyClose(float range)
 {
     /// enemy 1
-    if ( gladiator->game->getOtherRobotData(comp[0]).lifes > 0 && sqrt(sq(gladiator->game->getOtherRobotData(comp[0]).position.x - gladiator->robot->getData().position.x) + sq(gladiator->game->getOtherRobotData(comp[0]).position.y - gladiator->robot->getData().position.y)) < range)
+    if (comp[0] && gladiator->game->getOtherRobotData(comp[0]).lifes > 0 && sqrt(sq(gladiator->game->getOtherRobotData(comp[0]).position.x - gladiator->robot->getData().position.x) + sq(gladiator->game->getOtherRobotData(comp[0]).position.y - gladiator->robot->getData().position.y)) < range)
     {
         gladiator->log("enemy %d in range", comp[0]);
         return true;
         // }else if(comp2ID !=8 && comp2.lifes > 0 && distance(MyPosition(comp1.position.x, comp1.position.y), MyPosition(me.position.x, me.position.y) ) < range){
     }
-    else if ( gladiator->game->getOtherRobotData(comp[1]).lifes > 0 && sqrt(sq(gladiator->game->getOtherRobotData(comp[1]).position.x - gladiator->robot->getData().position.x) + sq(gladiator->game->getOtherRobotData(comp[1]).position.y - gladiator->robot->getData().position.y))  < range)
+    else if (comp[1] && gladiator->game->getOtherRobotData(comp[1]).lifes > 0 && sqrt(sq(gladiator->game->getOtherRobotData(comp[1]).position.x - gladiator->robot->getData().position.x) + sq(gladiator->game->getOtherRobotData(comp[1]).position.y - gladiator->robot->getData().position.y))  < range)
     {
         gladiator->log("enemy %d in range", comp[1]);
         return true;
